a53cb01e05f78bfbf6e0b2a3d49fa3d07641397b
Candace Moorer <candypattie@me.com>
2017-08-31 19:00:13 -0400

    updated working files

--- main.js
+++ main.js
@@ -1,31 +1,66 @@
-const form = document.querySelectorAll('form')[0]
- const baseUrl = 'https://itunes.apple.com/search?term='
- let results
+var form = document.querySelectorAll('form')[0]
+ var baseUrl = 'https://itunes.apple.com/search?term='
+ // const container =document.querySelector('.container');
+ var results
+
  form.addEventListener('submit', function (event) {
    event.preventDefault()
-   console.log(this)
-   const nameInputValue = event.target.querySelector('Prince').value
+  //  console.log(this)
+   const nameInputValue = event.target.querySelector('input').value
+  // const searchInput = document.querySelector('input[name="search"]').vaule
+
+  getSearchResults(nameInputValue).then(function (results) {
+      console.log(results)
+        event.target.querySelector('input[name="artist"]').value = ''
+        fetch("https://itunes.apple.com/search?term=").then(function(response) {
+            let songs = results
+              console.log(results);
+              var template = "<div>"
+             for(i=0; i < songs.results.length; i++) {
+               console.log(songs.results[i])
+               template +=   `<a>${songs.results[i].artistName}</a>
+                <a>${songs.results[i].trackViewUrl}</a>
+                <a>${songs.results[i].trackName}</a>
+                <a>${songs.results[i].artworkUrl100}</a>
+                `;
+                }
+                form.innerHTML = template;
 
 
 
-   getSearchResults(nameInputValue).then(function (results) {
-     results = event.target.querySelector('input[name="name"]').value
-     console.log(results)
-     })
+             return songs
+           })
+  })
+})
+
+function getSearchResults (artist) {
+  var template =`<li>${artistName}${trackViewUrl}${trackName}</li>`;
 
 
- function getSearchResults (searchTerms) {
-   return fetch(`${baseUrl}?q=${searchTerms}`).then(function (response) {
-     return response.json()
-   })
+   template += "</ul>";
+   container.innerHTML = template;
  }
 
- const searchInput = document.querySelector('input[name="search"]')
+ function getSearchResults (searchTerms) {
+   return fetch(`${baseUrl}${searchTerms}`).then(function (data) {
+     return data.json()
+   })
 
- searchInput.addEventListener('search', function (event) {
-   console.log(event.target.value)
- })
- })
+}
+var searchInput = document.querySelector('#search-btn')
+searchInput.addEventListener('submit', function (event) {
+    // console.log(event.target.value)
+  })
+// document.getElementById('myAudio').play();
+
+
+
+
+
+
+
+
+
 
 
 
