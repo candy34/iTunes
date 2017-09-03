1e6475df42692e81d17a5870374f9b861ca5333b
Candace Moorer <candypattie@me.com>
2017-09-03 19:39:11 -0400

    updated with playing track

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ var form = document.querySelectorAll('form')[0]
         fetch("https://itunes.apple.com/search?term=").then(function(response) {
             let songs = results
               console.log(results);
-              var template = "<div>"
+              var template = "<div class=row>"
              for(i=0; i < songs.results.length; i++) {
                console.log(songs.results[i])
-               template +=   `<a>${songs.results[i].artistName}</a>
-                <a>${songs.results[i].trackViewUrl}</a>
-                <a>${songs.results[i].trackName}</a>
-                <a>${songs.results[i].artworkUrl100}</a>
+               template +=   `<p>${songs.results[i].artistName}</p>
+                <audio play="true" controls="controls" src= "" onclick="play()" onclick="pause()"('${songs.results[i].trackViewUrl}')"></audio>
+                <p>${songs.results[i].trackName}</p>
+                <img src ="${songs.results[i].artworkUrl100}">
                 `;
                 }
                 form.innerHTML = template;
@@ -53,81 +53,8 @@ searchInput.addEventListener('submit', function (event) {
   })
 // document.getElementById('myAudio').play();
 
-
-
-
-
-
-
-
-
-
-
-
-// const form = document.querySelector('form')
-  //const baseUrl = 'https://itunes.apple.com/search?term='
-  //let results;
-  //form.addEventListener('submit', function (event) {
-  //  event.preventDefault()
-    //console.log(this)
-    //const nameInputValue = event.target.querySelector('input').value
-
-
-    //getSearchResults(nameInputValue).then(function (results) {
-
-      //results = event.target.querySelector('input[name="name"]').value
-      //console.log(results)
-    //  })
-  //})
-      //console.log(results);
-//  function getSearchResults (searchTerms) {
-  //  return fetch(`${baseUrl}?q=${searchTerms}`).then(function (response) {
-  //    return response.json()
-  //  })
-  //}
-
-//fetch("https://itunes.apple.com/search?term=")
-//.then(function(response) {
-  //if (response.status !==15) {
-  //  console.log(response.status);
-  //  return;
-  //}
-  //response.json().then(function(data) {
-  //  console.log("Here is the songs:", data);
-  //});
-//}
-//)
-//.catch(function(err) {
-  //  console.log("Fetch Error :-S", err);
-//});
-//function getArtist (artistName) {
-//  let chosenArtist = {}
-//  for (let i = 0; i < artist.length; i++) {
-//        if (artist[i].Name === artistName) {
-            //chosenArtist = artist[i]
-  //      }
-  //}
-//  return chosenArtist
-//}
-//   .then(function(data) {
-//     let songs = data.results
-//     console.log(songs);
-//     // for(i=0; i < songs.length; i++)
-//     {
-//
-//     }
-//     return songs
-//   })
-// })
-//songs.forEach(function (artistName) {
-  //  template += `<li>${artistName}</li>`;
-//});//
-
-//template += "</ul>";
-//container.innerHTML = template;
-
-  //const searchInput = document.querySelector('input')
-
-  //searchInput.addEventListener('search', function (event) {
-//    console.log(event.target.value)
-//  })
+// function on clicking play button
+function playsong (url){
+  var player = document.querySelector('audio')
+  player.setAttribute('src', url )
+}
